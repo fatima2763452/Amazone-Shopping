6cf2758e3f455ab47c5b818f6f58d72f8c139417
Fatima <youremail@example.com>
2025-07-10 10:03:35 +0100

    update link

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ const MONGO_URL = process.env.MONGO_URL;
 const formRoute = require("./Routes/FormRoute");
 const formTwoRoute = require("./Routes/FormTwoRoute")
 const allowedOrigins = [
-"https://vipul-project.onrender.com",
+"https://amazone-shopping-front.onrender.com",
 //  "http://localhost:3000"                                    
 ];
 const { FormModel }  =  require("./Model/FormModel")
