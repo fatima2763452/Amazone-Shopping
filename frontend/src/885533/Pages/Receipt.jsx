d0ec0e9db6cacc6313a0c5e5485599b25cb9b857
Fatima <youremail@example.com>
2025-11-02 18:04:39 +0000

    new token uo gjhfgh

--- frontend/src/885533/Pages/Receipt.jsx
+++ frontend/src/885533/Pages/Receipt.jsx
@@ -155,7 +155,7 @@ function Receipt() {
             }}
           >
             <h2>
-              RADHE BROKERAGE HOUSE
+              BHANSAVALI PVT LTD
               <br />
               <span style={{ fontSize: 12, fontWeight: 400 }}>Trade Exit Receipt</span>
             </h2>
